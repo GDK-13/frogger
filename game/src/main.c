85c63e093a81c89d604a9d4cf8e17809a148592e
GDK13 <kevingdkt@gmail.com>
2025-06-01 12:13:45 -0300

    day 1

--- game/src/main.c
+++ game/src/main.c
@@ -1,22 +1,50 @@
 #include "raylib.h"
- 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+
+Texture2D background;
+
 int main()
 {
-    InitWindow(800, 600, "raylib example");
+    const int screen_width = 448;
+    const int screen_height = 512;
+
+
+
+    InitWindow(screen_width, screen_height, "raylib example");
     SetTargetFPS(60);
+
+    background = LoadTexture("resources/bg/frogbreath_bg.png");
+
+
+    if (background.id == 0) {
+        printf("Erro ao carregar o background!\n");
+        CloseWindow();
+        return 1;
+    }
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         {
-        ClearBackground(RAYWHITE);
+            ClearBackground(RAYWHITE);
 
-        DrawText("hello raylib", 10, 10, 32, BLACK);
-
-    
+            DrawTexturePro(
+                background,
+                (Rectangle){0, 0, screen_width, screen_height},  // Origem da textura (src)
+                (Rectangle){0, 0, (float)GetScreenWidth(), (float)GetScreenHeight()}, // Destino da textura (dst)
+                (Vector2){0, 0},
+                0.0f,
+                WHITE
+            );
         }
         EndDrawing();
     }
+
+    UnloadTexture(background);
     CloseWindow();
 
     return 0;
