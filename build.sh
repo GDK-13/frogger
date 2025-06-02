2a4d2c2deb0cea5514c080f67b724498d8fa20f4
GDK13 <kevingdkt@gmail.com>
2025-06-02 16:20:57 -0300

    alpha0.1.1

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 # Caminhos
 SRC_DIR="game/src"
 BIN_DIR="game/bin/Debug"
@@ -29,4 +30,39 @@ if [ $? -eq 0 ]; then
     echo "🟢 Executável: $OUTPUT"
 else
     echo "❌ Falha na compilação."
+=======
+# Cria a pasta bin/Debug se não existir
+mkdir -p game/bin/Debug
+
+# Caminho de saída do executável
+OUTPUT="game/bin/Debug/frogger"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    gcc -o $OUTPUT \
+        game/src/main.c \
+        game/src/player.c \
+        -Igame/include -Igame/src \
+        -Lgame/bin/Debug \
+        -lraylib \
+        -lm -ldl -lpthread -lrt -lX11 \
+        -std=c17 -g -DPLATFORM_DESKTOP
+elif [[ "$OSTYPE" == "msys" ]]; then
+    OUTPUT="game/bin/Debug/frogger.exe"
+    gcc -o $OUTPUT \
+        game/src/main.c \
+        game/src/player.c \
+        -Igame/include -Igame/src \
+        -Lgame/bin/Debug \
+        -lraylib \
+        -std=c17 -g -DPLATFORM_DESKTOP
+fi
+
+if [ $? -eq 0 ]; then
+    echo "Build successful!"
+    echo "Running the game..."
+    cd game/bin/Debug
+    ./frogger
+else
+    echo "Build failed!"
+>>>>>>> 7758bc7 (alpha0.1.1)
 fi
