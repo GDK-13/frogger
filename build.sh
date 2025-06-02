7c2d6a82a28b4b6d9900bde95925629627daa561
GDK13 <kevingdkt@gmail.com>
2025-06-02 16:25:05 -0300

    alpha0.1.1

--- build.sh
+++ build.sh
@@ -1,36 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-# Caminhos
-SRC_DIR="game/src"
-BIN_DIR="game/bin/Debug"
-RAYLIB_DIR="raylib-master/src"
-
-# Nome do executável
-OUTPUT="$BIN_DIR/frogger"
-
-# Cria o diretório de saída se não existir
-mkdir -p "$BIN_DIR"
-
-# Compilação
-echo "🔧 Compilando o projeto..."
-gcc -o "$OUTPUT" \
-    $(find $SRC_DIR -name "*.c") \
-    -Igame/include -Igame/src \
-    -I$RAYLIB_DIR \
-    -I$RAYLIB_DIR/external \
-    -I$RAYLIB_DIR/external/glfw/include \
-    -L. -L$BIN_DIR -L$RAYLIB_DIR \
-    -lraylib -lm -ldl -lpthread -lrt -lX11 \
-    -std=c17 -g -DPLATFORM_DESKTOP
-
-# Resultado
-if [ $? -eq 0 ]; then
-    echo "✅ Compilado com sucesso!"
-    echo "🟢 Executável: $OUTPUT"
-else
-    echo "❌ Falha na compilação."
-=======
 # Cria a pasta bin/Debug se não existir
 mkdir -p game/bin/Debug
 
@@ -64,5 +33,4 @@ if [ $? -eq 0 ]; then
     ./frogger
 else
     echo "Build failed!"
->>>>>>> 7758bc7 (alpha0.1.1)
 fi
